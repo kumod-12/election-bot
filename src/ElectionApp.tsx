9bde66a53edaa76a8ab5e24aac859e6c68345021
kumod-12 <kumodmehta333@gmail.com>
2025-11-02 23:21:50 +0530

    Add files via upload

---
+++ src/ElectionApp.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ElectionChatbot from './ElectionChatbot';
+
+function ElectionApp() {
+  return <ElectionChatbot />;
+}
+
+export default ElectionApp;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './AppComponent';
+import ElectionApp from './ElectionApp';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ElectionApp />
   </React.StrictMode>
 );
