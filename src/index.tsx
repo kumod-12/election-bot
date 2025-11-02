8239e2f0687c084fd4d3243171429af560b362a2
kumod-12 <kumodmehta333@gmail.com>
2025-11-02 23:26:58 +0530

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import ElectionApp from './ElectionApp';
+import ElectionChatbot from './ElectionChatbot';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <ElectionApp />
+    <ElectionChatbot />
   </React.StrictMode>
 );
